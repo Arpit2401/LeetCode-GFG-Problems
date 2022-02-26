d43173ce34ebf4c7f014f014004f17dd6b28d5cd
Arpit Agarwal <56201652+Arpit2401@users.noreply.github.com>
2022-02-26 19:13:31 +0530

    Time: 0 ms (100.00%), Space: 42 MB (18.42%) - LeetHub

--- 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.java
+++ 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.java
@@ -4,7 +4,7 @@ class Solution {
 		int min = salary[0];
         int max = salary[0];
         int sum = salary[0];
-        for (int i = 1; i < n; ++i)
+        for (int i=1;i<n;++i)
         {
             if (salary[i] < min)
                 min = salary[i];
@@ -12,6 +12,6 @@ class Solution {
                 max = salary[i];
 			sum += salary[i];
         }
-        return (double)(sum - min - max) / (n - 2);
+        return (double)(sum - min - max)/(n - 2);
     }
 }
