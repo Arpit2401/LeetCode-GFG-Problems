1d494745555e85ed8e0ddd4d12f9ed416e2d8030
Arpit Agarwal <56201652+Arpit2401@users.noreply.github.com>
2022-02-28 00:08:12 +0530

    Time: 0 ms (100.00%), Space: 40.2 MB (37.23%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int climbStairs(int n) {
+        int ar[] = new int[n+1];
+        ar[0]=1;
+        ar[1]=1;
+        for(int i=2;i<=n;i++)
+            ar[i]=ar[i-1]+ar[i-2];
+        return ar[n];
+    }
+}
