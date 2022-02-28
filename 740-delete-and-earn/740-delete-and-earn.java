4f515b3a2e61fee51df7ec23f247d42f63119a4b
Arpit Agarwal <56201652+Arpit2401@users.noreply.github.com>
2022-03-01 01:50:47 +0530

    Time: 5 ms (57.71%), Space: 44.3 MB (33.71%) - LeetHub

---
+++ 740-delete-and-earn/740-delete-and-earn.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int deleteAndEarn(int[] nums) {
+        if(nums.length==1)
+            return nums[0];
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        for(int i : nums)
+        {
+            map.put(i, map.getOrDefault(i,0)+1);
+            if (i>max)
+                max = i;
+            if(i<min) 
+                min = i;
+        }
+        int x = 0;
+        int y = 0;
+        int ans = 0;
+        for(int i=min;i<=max;i++)
+        {
+            ans = (int)Math.max(x+map.getOrDefault(i,0)*i,y);
+            x = y;
+            y = ans;
+        }
+    return ans;
+    }
+}
