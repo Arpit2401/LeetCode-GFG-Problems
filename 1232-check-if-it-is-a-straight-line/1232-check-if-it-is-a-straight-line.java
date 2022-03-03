fb84048456077930782e2084935b9182051e096b
Arpit Agarwal <56201652+Arpit2401@users.noreply.github.com>
2022-03-03 10:05:58 +0530

    Time: 0 ms (100.00%), Space: 43.7 MB (38.70%) - LeetHub

---
+++ 1232-check-if-it-is-a-straight-line/1232-check-if-it-is-a-straight-line.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean checkStraightLine(int[][] coordinates) {
+        if(coordinates.length==2)
+            return(true);
+        int x1=coordinates[0][0],x2=coordinates[1][0],y1=coordinates[0][1],y2=coordinates[1][1];
+        for(int i=2;i<coordinates.length;i++)
+        {
+            if(((coordinates[i][1]-y1)*(coordinates[i][0]-x2))!=((coordinates[i][1]-y2)*(coordinates[i][0]-x1)))
+                return false;
+        }
+        return true;
+    }
+}
