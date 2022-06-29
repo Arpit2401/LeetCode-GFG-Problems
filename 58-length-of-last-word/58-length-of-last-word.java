a2401ddfc12b511711b4f14319c4a63918f0951c
Arpit Agarwal <56201652+Arpit2401@users.noreply.github.com>
2022-06-29 17:52:36 +0530

    Time: 0 ms (100.00%), Space: 40 MB (99.86%) - LeetHub

---
+++ 58-length-of-last-word/58-length-of-last-word.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        s=s.trim();
+        int ans = 0;
+        for(int i=s.length()-1;i>=0;i--)
+        {
+            if(s.charAt(i)==' ')
+                return ans;
+            ans++;
+        }
+        return ans++;
+    }
+}
