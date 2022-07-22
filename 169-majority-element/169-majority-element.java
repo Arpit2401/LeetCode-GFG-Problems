e07bf505c772fc5df55a91d16027e45be232bc61
Arpit Agarwal <56201652+Arpit2401@users.noreply.github.com>
2022-07-22 11:54:29 +0530

    Time: 4 ms (53.53%), Space: 56.4 MB (34.23%) - LeetHub

--- 169-majority-element/169-majority-element.java
+++ 169-majority-element/169-majority-element.java
@@ -1,6 +1,38 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        Arrays.sort(nums);
-        return nums[nums.length/2];
+        int cand = findCandidate(nums, nums.length);
+        if (isMajority(nums, nums.length, cand))
+            return cand;
+        return -1;
+    }
+  
+    int findCandidate(int a[], int size)
+    {
+        int maj_index = 0, count = 1;
+        int i;
+        for (i = 1; i < size; i++) {
+            if (a[maj_index] == a[i])
+                count++;
+            else
+                count--;
+            if (count == 0) {
+                maj_index = i;
+                count = 1;
+            }
+        }
+        return a[maj_index];
+    }
+    
+    boolean isMajority(int a[], int size, int cand)
+    {
+        int i, count = 0;
+        for (i = 0; i < size; i++) {
+            if (a[i] == cand)
+                count++;
+        }
+        if (count > size / 2)
+            return true;
+        else
+            return false;
     }
 }
