e53666ee69f71a54ef46e16fe19daaff7bb06945
Arpit Agarwal <56201652+Arpit2401@users.noreply.github.com>
2022-07-22 11:58:35 +0530

    Time: 14 ms (29.65%), Space: 55.6 MB (65.15%) - LeetHub

--- 169-majority-element/169-majority-element.java
+++ 169-majority-element/169-majority-element.java
@@ -1,38 +1,12 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        int cand = findCandidate(nums, nums.length);
-        if (isMajority(nums, nums.length, cand))
-            return cand;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i : nums)
+        {
+            map.put(i,map.getOrDefault(i,0)+1);
+            if(map.get(i)>nums.length/2)
+                    return i;
+        }
         return -1;
-    }
-  
-    int findCandidate(int a[], int size)
-    {
-        int maj_index = 0, count = 1;
-        int i;
-        for (i = 1; i < size; i++) {
-            if (a[maj_index] == a[i])
-                count++;
-            else
-                count--;
-            if (count == 0) {
-                maj_index = i;
-                count = 1;
-            }
-        }
-        return a[maj_index];
-    }
-    
-    boolean isMajority(int a[], int size, int cand)
-    {
-        int i, count = 0;
-        for (i = 0; i < size; i++) {
-            if (a[i] == cand)
-                count++;
-        }
-        if (count > size / 2)
-            return true;
-        else
-            return false;
     }
 }
