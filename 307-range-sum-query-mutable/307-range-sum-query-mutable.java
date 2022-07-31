5f410ae09b70bb5895815cb1506bb485b2d45a88
Arpit Agarwal <56201652+Arpit2401@users.noreply.github.com>
2022-07-31 23:45:29 +0530

    Time: 135 ms (63.57%), Space: 136 MB (22.82%) - LeetHub

---
+++ 307-range-sum-query-mutable/307-range-sum-query-mutable.java
@@ -0,0 +1,61 @@
+class NumArray {
+  static class BinaryIndexedTree {
+    int[] nums;
+    int[] BIT;
+    int n;
+
+    public BinaryIndexedTree(int[] nums) {
+      this.nums = nums;
+      this.n = nums.length;
+      BIT = new int[n + 1];
+      for (int i = 0; i < n; i++) {
+        init(i, nums[i]);
+      }
+    }
+
+    void init(int i, int val) {
+      i++;
+      while (i <= n) {
+        BIT[i] += val;
+        i += (i & -i);
+      }
+    }
+
+    void update(int i, int val) {
+      int diff = val - nums[i];
+      nums[i] = val;
+      init(i, diff);
+    }
+
+    int getSum(int i) {
+      i++;
+      int ret = 0;
+      while (i > 0) {
+        ret += BIT[i];
+        i -= (i & -i);
+      }
+      return ret;
+    }
+  }
+
+  BinaryIndexedTree binaryIndexedTree;
+
+  public NumArray(int[] nums) {
+    binaryIndexedTree = new BinaryIndexedTree(nums);
+  }
+
+  public void update(int index, int val) {
+    binaryIndexedTree.update(index, val);
+  }
+
+  public int sumRange(int left, int right) {
+    return binaryIndexedTree.getSum(right) - binaryIndexedTree.getSum(left - 1);
+  }
+}
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray obj = new NumArray(nums);
+ * obj.update(index,val);
+ * int param_2 = obj.sumRange(left,right);
+ */
